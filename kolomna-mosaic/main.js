2305e4ca9ebffd33a886e864bc55650a7d47b890
online-advertising-local <online-advertising-local@wildberries.ru>
2022-06-03 17:20:11 +0300

    add walls kolomna

---
+++ kolomna-mosaic/main.js
@@ -0,0 +1,215 @@
+const mosaicMarkers = [
+    {
+        coords: [55.10485525586531, 38.76542069943753],
+        img: 'https://sun6-21.userapi.com/s/v1/ig2/znHBETdLoiaNw61bbsHCukZ0sNfqcIn14QsoTVdlLHWg1QVmM2LHuyebvI1f35vPPAI-Zdd2JAoHLRUpEBT77u5D.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'Динозавр'
+    },
+    {
+        coords: [55.10452112966388, 38.76606613397599],
+        img: 'https://sun9-36.userapi.com/s/v1/ig2/_dAZrnGNvkcDcxnbTmPYxTVilqTteMJ6OPcTogUq-zjX40cKTobWkIjQXmZQrCWEeMgi2fy-ZTxpcH5ccaQlloeG.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'Маяк'
+    },
+    {
+        coords: [55.104728278693074, 38.76819849014283],
+        img: 'https://sun9-38.userapi.com/s/v1/ig2/pul867ZIaxvGQF7ZqHXTJ6ozNhOKc86NIZzfzLfPs0c61RiXQt8Ek5Q9x7C8LhZgfR3-DYomE24n4-qGe8RSq81u.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'Мышь'
+    },
+    {
+        coords: [ 55.104029110780004, 38.76969456392545],
+        img: 'https://sun9-46.userapi.com/s/v1/ig2/4Sc-cin_0WKCOF7M8PXutJbcx3gC1VZTHFY9hsjcu-RQOX-092Vf02szfyRIFg9nPzhKP6KmiMBmqXd2CNmO0UUK.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'Черепашка'
+    },
+    {
+        coords: [ 55.100711401373474, 38.766892323601496],
+        img: 'https://sun6-23.userapi.com/s/v1/ig2/lPS4hl7Y6D4q71I4lOpD2DTmPRj2wXzpaX9YpregZSfNE33onjNc44x_l_VpOs8bNsfEa431uu-fxuQEZvTH2K9P.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'фабрика'
+    },
+    {
+        coords: [55.1008606243339,  38.76587343549546],
+        img: 'https://sun9-55.userapi.com/s/v1/ig2/tsCZ_7X2MzCxrAAEkns438dRLHxLWAMyE48Xy1qgzGzZSQJj9XQCRGNlGjL0rKwhzdialLuXjPlJdnhTjoZB_LQN.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'вход1'
+    },
+    {
+        coords: [55.10199996320048,  38.7675641477108],
+        img: 'https://sun9-82.userapi.com/s/v1/ig2/LPpwd3pbZqs56ElN2fH_pegCR0J_hQr9WaBdlBtfZGeYzTHiyLTy9PGEWTGdsPctcDdWGBLQjFvppBB8e8at47qf.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'вход камера'
+    },
+    {
+        coords: [55.102143993510815, 38.76775627926691],
+        img: 'https://sun9-83.userapi.com/s/v1/ig2/vUhNlEV24AWXFo6a6ADM9DUDDXNlSQiBdYHOBti12cLAKkqAEyOg-CUSFYlO7kSzpjOJub1ZXO-PT3IDRqxFXgNQ.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'вход 2'
+    },
+    {
+        coords: [55.10115874900605,  38.76503013811089],
+        img: 'https://sun9-79.userapi.com/s/v1/ig2/G034hOqh73ir1F__-xeVXjhWgUYQxHkEihEyG_q9Kp8rsZ2IVKWWiZPBSrNn0MJB11AcvKoOjiviaFc6lQfrFqIW.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'зеленое'
+    },
+    {
+        coords: [55.10143036703157, 38.76452051839806],
+        img: 'https://sun9-68.userapi.com/s/v1/ig2/T109UbLwmf5OyPZrr5CPATwzNlBs2U2PY1YJ5OzdlZR1-7GK-deyKdMYi6O_wQxf4O-tG18ql-F93t3EJ837JFzS.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'кирпичное'
+    },
+    {
+        coords: [55.100823286697974,  38.763282846739465],
+        img: 'https://sun9-16.userapi.com/s/v1/ig2/tblEbBsHOukXpxhx4VPQJPircQDDnuWtFwX7LGPdMYv2wu5L0oqbZCO_xImrHdFUrBVOMKbP3C4yePisv3_XSP5-.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'вставки'
+    },
+    {
+        coords: [55.10026230290336, 38.76223025896252],
+        img: 'https://sun9-75.userapi.com/s/v1/ig2/NOAPiUp-05eBgIWUqwfIxf0_BoHP-KfIxk4Cd3r27O4XJqXQgA_6vIMZTzYfm8zhTLmESHEXKV3TSvvhGUOnK0XQ.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'зал'
+    },
+    {
+        coords: [55.1012372905219, 38.76026988029481],
+        img: 'https://sun9-38.userapi.com/s/v1/ig2/1at6S_WtP6II8XOVl5U-fM2V_tqfJW6aR6cEVOd-TRI7g4zbvdl6JfLItyqatdaye3RWOoUDG4hANtI78RKcUo4v.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'недалеко от сибаса вставки'
+    },
+    {
+        coords: [ 55.101195452700736, 38.759099932121906],
+        img: 'https://sun9-88.userapi.com/s/v1/ig2/4oIv2YVdn2cKo0KMfYH2eZaZJv7SUxmXieujRvBNu_qPcm9JGHxJr3UDGV1T9jnVpvPy7GLznlZ5Ij3NBqzqTEE3.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'сибас'
+    },
+    {
+        coords: [ 55.10403462660368, 38.75570451171371],
+        img: 'https://sun9-18.userapi.com/s/v1/ig2/3ONPJ6jEVSen1lnNarhaldaR503HsxxInhtEzeyMG4hx3rSVFXXPc2WnmNxw19iguk99WFW4Y_7yOJ-SLqVnmmoR.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'одинокое здание'
+    },
+
+    {
+        coords: [55.103102295251105, 38.75697882423993],
+        img:'https://sun9-5.userapi.com/s/v1/ig2/47cPgWQGVObTWzAOG_vEOMmMriOYp0rVQH_zLSBfKBsgTxOiMZBEUu75sN1b9bnyxtLSekRPvSVBd9IbG7TmVsxL.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'стена кремля'
+    },
+    {
+        coords: [55.10143482021943, 38.759525934328856],
+        img: 'https://sun9-57.userapi.com/s/v1/ig2/XIFmwI9Dcq1MONF9B4G5e-CMqWr4Q9yxWvzDo8BCduiDxRYzyW2wMBdf_SM0p9_bWGOXPyDX361xjP_fRIKOVgib.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'мис точка'
+    },
+    {
+        coords: [55.106035397137546, 38.75636047765511],
+        img: 'https://sun9-12.userapi.com/s/v1/ig2/Vc7f-uGk8z_ytE9241Yfj8HAeIERVT6mhlG68o4QS4RO1a8Uuiz6zMhnlNEvXv3oLiUyaJ4r5cE9J1_dDui6eoGE.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'падающее здание'
+    },
+    {
+        coords: [55.100847160095256, 38.77069535923511],
+        img: 'https://sun9-10.userapi.com/s/v1/ig2/J7zDYIUamTmyNsp2KSfbMDOd8CMxRPaaJg66BUMjUg5WVHw0WFEJzC1RUzDts2BsxkZo4bfkN7IQWIPSnKIWCISI.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'столбики'
+    },
+    {
+        coords: [51.666202887749236, 39.202910121166504],
+        popupText: 'Папоротник 2'
+    },
+    {
+        coords: [51.669105156554934, 39.20219600200654],
+        popupText: 'Акула'
+    },
+    {
+        coords: [51.67141689449062, 39.20254540491442],
+        popupText: 'Рыбы'
+    },
+    {
+        coords: [51.67241209706212, 39.20243740081788],
+        popupText: 'Ракета'
+    },
+    {
+        coords: [51.70780412117194, 39.148741685075954],
+        popupText: 'Кость'
+    },
+    {
+        coords: [51.67269650975726, 39.19625733749854],
+        popupText: 'Ящерица'
+    },
+    {
+        coords: [51.67388800148916, 39.21014045397897],
+        popupText: 'Папоротник 3'
+    },
+    {
+        coords: [ 51.669227847990165,  39.195093081325474],
+        popupText: 'Гусь'
+    },
+    {
+        coords: [51.666813611257304, 39.197828024625785],
+        popupText: 'Цыплята'
+    },
+    {
+        coords: [51.657339274296156, 39.20341908931733],
+        popupText: 'Лягушка'
+    },
+    {
+        coords: [51.66677840208671, 39.207654025758536],
+        popupText: 'Горы'
+    },
+    {
+        coords: [51.66279585187353, 39.207250469780774],
+        popupText: 'Хи-хи'
+    },
+    {
+        coords: [51.6643834039455, 39.21128744531416],
+        popupText: 'Таракан'
+    },
+    {
+        coords: [51.67300426011623, 39.18474018573762],
+        popupText: 'Слова'
+    },
+    {
+        coords: [ 51.67428666373837, 39.189204114954926],
+        popupText: 'Радуга'
+    },
+    {
+        coords: [ 51.67223695241179, 39.19073187593313],
+        popupText: 'Любовь'
+    },
+    {
+        coords: [ 51.67133870930307, 39.188682668245754],
+        popupText: 'Кринж'
+    },
+    {
+        coords: [51.67145182237857, 39.18852173570486],
+        popupText: 'Мышь'
+    },
+    {
+        coords: [51.66871755667519, 39.20455366373063],
+        popupText: 'Мишка'
+    },
+];
+
+
+class Map {
+
+    map;
+
+    constructor() {
+        this.createMap();
+    }
+
+    createMap() {
+        this.map = L.map('mapid').setView([55.101298681409645, 38.76795403193683], 14);
+
+        this.map.on('click', (ev) => {
+            console.log(ev.latlng)
+        });
+
+        var gl = L.mapboxGL({
+            attribution: "\u003ca href=\"https://www.instagram.com/al_kachino_art/\" target=\"_blank\"\u003e\u0026copy; Instagram\u003c/a\u003e  ",
+            style: 'https://api.maptiler.com/maps/streets/style.json?key=gsrsyy45DnpndozB6p1n'
+        }).addTo(this.map);
+    }
+
+    drawMarkers() {
+        mosaicMarkers.forEach(marker => {
+            const newMarker = L.marker(marker.coords).addTo(this.map);
+            const popup = L.popup()
+                .setLatLng(marker.coords)
+                .setContent('<img src="' + marker.img + '" alt="">');
+            newMarker.bindPopup(popup);
+
+        })
+    }
+}
+
+
+function init() {
+    const map = new Map();
+    map.drawMarkers();
+
+}
