b21ccb64e10c6da32aca37942a199e7ae8478be9
online-advertising-local <online-advertising-local@wildberries.ru>
2022-06-03 17:37:09 +0300

    update map

--- kolomna-mosaic/main.js
+++ kolomna-mosaic/main.js
@@ -96,81 +96,25 @@ const mosaicMarkers = [
         popupText: 'столбики'
     },
     {
-        coords: [51.666202887749236, 39.202910121166504],
-        popupText: 'Папоротник 2'
+        coords: [55.09717744830971, 38.762883029467055],
+        img: 'https://sun9-78.userapi.com/s/v1/ig2/vkDCUIgu8eFWgJ2JO-2uZHGOIsoQEDrk7RJkNFMvONtLG5MgLqAWPq66c07w4tFV9VW-TLRvFnqMwZdnQ4XRsY8x.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'забор стена остановка'
     },
     {
-        coords: [51.669105156554934, 39.20219600200654],
-        popupText: 'Акула'
+        coords: [55.097853019523484, 38.76667753946095],
+        img: 'https://sun9-21.userapi.com/s/v1/ig2/QiAUTQm0nZ5U5VcHBB5HQhqLoYbXXnzUJ8AclwrR0CLzIRVA_sDIeiNLCdYDob0G7acZ76U3YwL-EXrfotsQZW2_.jpg?size=1440x1920&quality=95&type=album',
+        popupText: 'голубенькое'
     },
     {
-        coords: [51.67141689449062, 39.20254540491442],
+        coords: [55.097637755852794, 38.766364216799055],
+        img: 'https://sun9-1.userapi.com/s/v1/ig2/zxNRszJAMgoVyBIzI54_VL9RRFZAXVoG1JtZosXpKy0nvSbjWOPzBGKy7y4L7Zegi6JV7QyaVE2r2v5_vLWjLBAr.jpg?size=1440x1920&quality=95&type=album',
         popupText: 'Рыбы'
     },
-    {
-        coords: [51.67241209706212, 39.20243740081788],
-        popupText: 'Ракета'
-    },
-    {
-        coords: [51.70780412117194, 39.148741685075954],
-        popupText: 'Кость'
-    },
-    {
-        coords: [51.67269650975726, 39.19625733749854],
-        popupText: 'Ящерица'
-    },
-    {
-        coords: [51.67388800148916, 39.21014045397897],
-        popupText: 'Папоротник 3'
-    },
-    {
-        coords: [ 51.669227847990165,  39.195093081325474],
-        popupText: 'Гусь'
-    },
-    {
-        coords: [51.666813611257304, 39.197828024625785],
-        popupText: 'Цыплята'
-    },
-    {
-        coords: [51.657339274296156, 39.20341908931733],
-        popupText: 'Лягушка'
-    },
-    {
-        coords: [51.66677840208671, 39.207654025758536],
-        popupText: 'Горы'
-    },
-    {
-        coords: [51.66279585187353, 39.207250469780774],
-        popupText: 'Хи-хи'
-    },
-    {
-        coords: [51.6643834039455, 39.21128744531416],
-        popupText: 'Таракан'
-    },
-    {
-        coords: [51.67300426011623, 39.18474018573762],
-        popupText: 'Слова'
-    },
-    {
-        coords: [ 51.67428666373837, 39.189204114954926],
-        popupText: 'Радуга'
-    },
-    {
-        coords: [ 51.67223695241179, 39.19073187593313],
-        popupText: 'Любовь'
-    },
-    {
-        coords: [ 51.67133870930307, 39.188682668245754],
-        popupText: 'Кринж'
-    },
-    {
-        coords: [51.67145182237857, 39.18852173570486],
-        popupText: 'Мышь'
-    },
-    {
-        coords: [51.66871755667519, 39.20455366373063],
-        popupText: 'Мишка'
-    },
+
+    // {
+    //     coords: [51.66871755667519, 39.20455366373063],
+    //     popupText: 'Мишка'
+    // },
 ];
 
 
@@ -186,7 +130,7 @@ class Map {
         this.map = L.map('mapid').setView([55.101298681409645, 38.76795403193683], 14);
 
         this.map.on('click', (ev) => {
-            console.log(ev.latlng)
+            // console.log(ev.latlng)
         });
 
         var gl = L.mapboxGL({
