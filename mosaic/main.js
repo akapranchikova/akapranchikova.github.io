0c57f75bc6da734aa5a4a288389601243755d1fd
Alisa Kapranchikova <kapranchikovaal@gmail.com>
2023-07-25 09:17:10 +0200

    add new mosaic

--- mosaic/main.js
+++ mosaic/main.js
@@ -1,12 +1,13 @@
 const mosaicMarkers = [
     {
         coords: [51.65678496490777, 39.187461403671485],
-        popupText: 'Динозавр'
+        popupText: 'Динозавр',
+        img: ''
     },
-    {
-        coords: [51.6683336583688, 39.20387894164379],
-        popupText: 'Маяк'
-    },
+    // {
+    //     coords: [51.6683336583688, 39.20387894164379],
+    //     popupText: 'Маяк'
+    // },
     {
         coords: [51.66283980817729, 39.19396239451089],
         popupText: 'Мышь'
@@ -27,10 +28,10 @@ const mosaicMarkers = [
         coords: [51.653756238695806, 39.19355711852405],
         popupText: 'Ляшушка'
     },
-    {
-        coords: [51.65759510514901, 39.1970680191298],
-        popupText: 'Нож'
-    },
+    // {
+    //     coords: [51.65759510514901, 39.1970680191298],
+    //     popupText: 'Нож'
+    // },
     {
         coords: [51.665963975585875, 39.201407100226795],
         popupText: 'Заяц'
@@ -47,28 +48,26 @@ const mosaicMarkers = [
         coords: [51.669123361160494, 39.22261369341869],
         popupText: 'Огрызок'
     },
-    {
-        coords: [51.65980270815523, 39.189626262141246],
-        popupText: 'Яблоко'
-    },
-    {
-        coords: [51.66196694696772, 39.19144453628178],
-        popupText: 'Крокодил'
-    },
+    // {
+    //     coords: [51.65980270815523, 39.189626262141246],
+    //     popupText: 'Яблоко'
+    // },
+    // {
+    //     coords: [51.66196694696772, 39.19144453628178],
+    //     popupText: 'Крокодил'
+    // },
     {
         coords: [51.67233857452443, 39.199979780150905],
         popupText: 'Папоротник'
     },
-
-
     {
         coords: [51.670377545356764, 39.190699659957325],
         popupText: 'Пчелы'
     },
-    {
-        coords: [51.664914524319464, 39.19884252663906],
-        popupText: 'Яичница'
-    },
+    // {
+    //     coords: [51.664914524319464, 39.19884252663906],
+    //     popupText: 'Яичница'
+    // },
     {
         coords: [51.665672201281964, 39.19950097293623],
         popupText: 'Таракан'
@@ -81,18 +80,18 @@ const mosaicMarkers = [
         coords: [51.666202887749236, 39.202910121166504],
         popupText: 'Папоротник 2'
     },
-    {
-        coords: [51.669105156554934, 39.20219600200654],
-        popupText: 'Акула'
-    },
+    // {
+    //     coords: [51.669105156554934, 39.20219600200654],
+    //     popupText: 'Акула'
+    // },
     {
         coords: [51.67141689449062, 39.20254540491442],
         popupText: 'Рыбы'
     },
-    {
-        coords: [51.67379767993757, 39.203378856182105],
-        popupText: 'Ракета'
-    },
+    // {
+    //     coords: [51.67379767993757, 39.203378856182105],
+    //     popupText: 'Ракета'
+    // },
     {
         coords: [51.70780412117194, 39.148741685075954],
         popupText: 'Кость'
@@ -117,10 +116,10 @@ const mosaicMarkers = [
         coords: [51.657339274296156, 39.20341908931733],
         popupText: 'Лягушка'
     },
-    {
-        coords: [51.66677840208671, 39.207654025758536],
-        popupText: 'Горы'
-    },
+    // {
+    //     coords: [51.66677840208671, 39.207654025758536],
+    //     popupText: 'Горы'
+    // },
     {
         coords: [51.66279585187353, 39.207250469780774],
         popupText: 'Хи-хи'
@@ -133,32 +132,38 @@ const mosaicMarkers = [
         coords: [51.67300426011623, 39.18474018573762],
         popupText: 'Слова'
     },
-    {
-        coords: [51.67428666373837, 39.189204114954926],
-        popupText: 'Радуга'
-    },
-    {
-        coords: [51.67223695241179, 39.19073187593313],
-        popupText: 'Любовь'
-    },
-    {
-        coords: [51.67133870930307, 39.188682668245754],
-        popupText: 'Кринж'
-    },
+    // {
+    //     coords: [51.67428666373837, 39.189204114954926],
+    //     popupText: 'Радуга',
+    //     throwAway: true
+    // },
+    // {
+    //     coords: [51.67223695241179, 39.19073187593313],
+    //     popupText: 'Любовь',
+    //     img: 'https://sun9-19.userapi.com/impg/B-fTxa026P8Kra-nApadTD-FonFoJO4KsOqfrQ/fFaUrEP9YAw.jpg?size=1440x1795&quality=96&sign=3c9c611a961ad8331ef4cf64936c5301&type=album',
+    //     throwAway: true
+    // },
+    // {
+    //     coords: [51.67133870930307, 39.188682668245754],
+    //     popupText: 'Кринж'
+    // },
     {
         coords: [51.67145182237857, 39.18852173570486],
-        popupText: 'Мышь'
+        popupText: 'Мышь',
+        img: 'https://sun9-51.userapi.com/impg/xNVGwWwQ6vpBnz9UoSDGbZqA06xvUBI7WsC9wA/ZpZsdjB53fM.jpg?size=1280x960&quality=96&sign=130dd8869c49358fdd7fe9aab454b440&type=album'
     },
-    {
-        coords: [51.66871755667519, 39.20455366373063],
-        popupText: 'Мишка'
-    },
+    // {
+    //     coords: [51.66871755667519, 39.20455366373063],
+    //     popupText: 'Мишка',
+    //     img: 'https://sun9-63.userapi.com/impg/s_1m1uOw1VwgdSuaBihYMSfV9tqjM1bmlfSL5g/ZCudkOWjdTY.jpg?size=1440x1800&quality=96&sign=e8ff391f775fa1ec1af87e8332b5ef1f&type=album'
+    // },
 
 
     // Коломна
     {
         coords: [55.10200303227641, 38.767566788010306],
-        popupText: 'Камера'
+        popupText: 'Камера',
+        img: 'https://sun9-43.userapi.com/impg/tkiArxuMYILkLO9NuZP72rHQ_cYjx7LhCImhEw/nUJwu16E_co.jpg?size=1620x2160&quality=96&sign=e9aeba77aa0c67db1da4b27d96d71335&type=album'
     },
     {
         coords: [55.10026040631906, 38.762172336491865],
@@ -169,28 +174,67 @@ const mosaicMarkers = [
         popupText: 'Динозавр'
     },
     {
-        coords: [ 55.10032792887418, 38.762020791682524],
-        popupText: 'Корова'
+        coords: [55.10032792887418, 38.762020791682524],
+        popupText: 'Корова',
+        img: 'https://sun9-69.userapi.com/impg/SYsfyzlSC_jDp_6eqL0cRy4VBrGfHCklaq-ZFQ/-mEK0shiIgQ.jpg?size=1620x2160&quality=96&sign=f8b611f3fa51dcbac7710b229f8443d2&type=album'
     },
-    {
-        coords: [55.101370707411704, 38.75937046295804],
-        popupText: 'Земля'
-    },
-    {
-        coords: [55.10144292201307, 38.759492364479236],
-        popupText: 'Красная собака'
-    },
-    {
-        coords: [55.10152271902324, 38.75963854487055],
-        popupText: 'Жираф'
-    },
+    // {
+    //     coords: [55.101370707411704, 38.75937046295804],
+    //     popupText: 'Земля'
+    // },
+    // {
+    //     coords: [55.10144292201307, 38.759492364479236],
+    //     popupText: 'Красная собака',
+    //     img: 'https://sun9-79.userapi.com/impg/0rSosA8pzut2Fnfafdei1vBEFQQ0kOF-WbCDJw/quzsNegSCNY.jpg?size=1440x1800&quality=96&sign=0d4ab2a1ff5aab2874a71ab7137fccfd&type=album'
+    // },
+    // {
+    //     coords: [55.10152271902324, 38.75963854487055],
+    //     popupText: 'Жираф'
+    // },
     {
         coords: [55.10067423648999, 38.75815230558146],
         popupText: 'Маленький человек на столбе'
     },
     {
         coords: [55.7556526798057, 37.64268911077125],
-        popupText: 'Зебра'
+        popupText: 'Зебра',
+        img: 'https://sun9-11.userapi.com/impg/Zgq6dH5054rquFm9eYYv4ShVpKIk_CYWCareeQ/BMD33Uxg_Ag.jpg?size=1440x1440&quality=96&sign=8457ef55bc7288142f8c921d2e8d56f9&type=album'
+    },
+    {
+        coords: [51.671801464866576, 39.209166583896156],
+        popupText: 'Взрыв',
+    },
+    {
+        coords: [51.67013810623422, 39.21108985021421],
+        popupText: 'Волк серый',
+    },
+    {
+        coords: [51.67021137831506, 39.208115637302406],
+        popupText: 'Ежик',
+    },
+    {
+        coords: [51.67051418066348, 39.20577143082483],
+        popupText: 'Искры',
+    },
+    {
+        coords: [51.670841616552444, 39.203656614608924],
+        popupText: 'Тучка',
+    },
+    {
+        coords: [51.67240169092245, 39.20708596862824],
+        popupText: 'Акула',
+    },
+    {
+        coords: [51.66574473832743, 39.209303855896],
+        popupText: 'Рыбка',
+    },
+    {
+        coords: [51.66668143340551, 39.205773114899785],
+        popupText: 'Бургер',
+    },
+    {
+        coords: [51.66658328085882, 39.206411480645336],
+        popupText: 'Кукуруза',
     },
 ];
 
@@ -198,6 +242,13 @@ const VORONEZH_CENTER = [51.6683, 39.1919];
 const COLOMNA_CENTER = [55.10204293024229, 38.76281261444092];
 const MOSCOW_CENTER = [55.7556526798057, 37.64268911077125];
 let mapObj;
+
+window.mobileCheck = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
+
 class Map {
 
     map;
@@ -217,7 +268,7 @@ class Map {
         this.map = L.map('mapid').setView([51.6683, 39.1919], 14);
 
         this.map.on('click', (ev) => {
-            // console.log(ev.latlng)
+            console.log(ev.latlng)
         });
 
         var gl = L.mapboxGL({
